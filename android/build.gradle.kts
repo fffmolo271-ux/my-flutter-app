b8ea4168d5600ff1777ee827e174191af5cbf7b1
freha <fffmolo271@gmail.com>
2026-06-06 11:24:36 +0300

    update kotlin to 1.9.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -19,3 +25,4 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
